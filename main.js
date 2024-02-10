dd5650da2b60c069c0837ce065635cbadfda6503
{leviathan} <117519316+webdevleviathan@users.noreply.github.com>
2024-02-10 10:51:21 +0200

    Update main.js
    learned about public, private and protected classes

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-function Persoana(nume, prenume){
-    this.nume = nume;
-    this.prenume = prenume;
+class Persoana {
+    #nume;
+    #prenume;
+    #dataNasterii;
+    
+    constructor(nume, prenume, data) {
+        if (nume === undefined || prenume === undefined || data === undefined) {
+            throw new Error('Error, Invalid arguments');
+        }
+        this.#nume = nume;
+        this.#prenume = prenume;
+        this.#dataNasterii = data;
+    }
+    
+    get nume() {
+        return this.#nume;
+    }
+    
+    get prenume() {
+        return this.#prenume;
+    }
+    
+    get dataNasterii() {
+        return this.#dataNasterii;
+    }
+    
+    salut() {
+        console.log(`Salut, ma numesc ${this.#nume} ${this.#prenume}, 'inginer'`);
+    }
 }
 
-Persoana.prototype.salut = function(){
-    console.log(`salut. ma numesc ${this.name} $this.prenume`);
-};
-
-function Angajat(nume, prenume, meserie){
-    Persoana.call(this, nume, prenume);
-    this.meserie = meserie;
+class Angajat extends Persoana {
+    #salariu;
+    
+    constructor(nume, prenume, data, meserie) {
+        super(nume, prenume, data);
+        this.meserie = meserie;
+        this.#salariu = 100;
+    }
+    
+    get salariu() {
+        return this.#salariu;
+    }
+    
+    salut() {
+        console.log(`Salut, ma numesc ${this.nume} ${this.prenume} si sunt ${this.meserie}`);
+    }
 }
 
-Angajat.prototype = Object.create(Persoana.prototype);
+const a = new Angajat('Leviathan', 'Xan', new Date('1999/05/02'), 'inginer');
+console.log(a.nume, a.prenume);
+a.salut();
 
-Angajat.prototype.primesteSalariu = function(){
-console.log('am primit salariul');
-};
+const p = new Persoana('Ionescu', 'Diana', new Date('1998/02/02'));
+p.salut();
 
-Angajat.prototype.salut = function(){
-    console.log(`salut, ma numesc ${this.nume} ${this.prenume} si sunt ${this.meserie}.`);
-}
-
-const p = new Persoana('Leviathan', 'Xan');
-console.log(p);
-const a = new Angajat('Tanase', 'Anca', 'profesoara');
-console.log(a);
-
-//clasa derivata este specializarea clasei de baza
-//functiile noi definite doar in clasa derivata
+// Public: Accessible from anywhere.
+// Private: Accessible only within the class where it is declared.
+// Protected: Accessible within the class where it is declared and its subclasses.
